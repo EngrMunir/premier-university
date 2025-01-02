543208c020c2abb055b72ec0c350f52ffbfe9883
munir <smsirajulmonir@gmail.com>
2025-01-02 18:55:48 +0600

    student interface and student schema ready

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 export const app:Application = express()
 const port = 3000
+
+// parsers
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
-
-console.log(process.cwd());
