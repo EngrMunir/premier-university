801aa0c4a8ff63d8e25b515952c661d44f797bc4
munir <smsirajulmonir@gmail.com>
2025-01-05 11:47:03 +0600

    builtin validator applied

--- src/modules/student/student..model.ts
+++ src/modules/student/student..model.ts
@@ -4,14 +4,14 @@ import { TGuardian, TLocalGuardian, TStudent, TUserName } from "./student.interf
 const userNameSchema = new Schema<TUserName>({
         firstName:{
             type:String,
-            required:true,
+            required:[true,'First name is required'],
         },
         middleName:{
             type:String,
         },
         lastName:{
             type:String,
-            required:true,
+            required:[true,'Last name is required'],
         },
 })
 
@@ -19,27 +19,27 @@ const guardianSchema = new Schema<TGuardian>(
     {
         fatherName:{
             type:String,
-            required:true,
+            required:[true,'Father name is required'],
         },
         fatherOccupation:{
             type:String,
-            required:true
+            required:[true,'Father occupation is required']
         },
         fatherContactNo:{
             type:String,
-            required:true,
+            required:[true,'Father contact no is required'],
         },
         motherName:{
             type:String,
-            required:true,
+            required:[true,'Mother name is required'],
         },
         motherOccupation:{
             type:String,
-            required:true
+            required:[true,'Mother occupation is required']
         },
         motherContactNo:{
             type:String,
-            required:true,
+            required:[true,'Mother contact no is required'],
         },
     }
 )
@@ -48,38 +48,61 @@ const localGuardianSchema = new Schema<TLocalGuardian>(
     {
         name:{
             type:String,
-            required:true,
+            required:[true,'Local guardian name is required'],
         },
         occupation:{
             type:String,
-            required:true
+            required:[true,'Local guardian occupation is required'],
         },
         contactNo:{
             type:String,
-            required:true
+            required:[true,'Local guardian contact name is required'],
         },
         address:{
             type:String,
-            required:true
+            required:[true,'Local guardian address is required'],
         }
     }
 )
 
 const studentSchema = new Schema<TStudent>({
-    id:{ type:String },
-    name:userNameSchema,
-    gender:["male","female"],
+    id:{ type:String, required:true, unique:true },
+    name:{
+        type:userNameSchema,
+        required:[true,'name is required'],
+    },
+    gender:{
+        type:String,
+        enum:{
+            values:["male","female","other"],
+            message:`{VALUE} is not required`
+        },
+        required:true
+    },
     dateOfBirth:{ type: String },
-    email: { type: String, required:true },
-    contactNo:{ type:String, required:true},
-    emergencyContactNo:{ type:String, required:true},
-    bloodGroup:['A+','A-','B+','B-','AB+','AB-','O+','O-'],
-    presentAddress:{ type:String, required:true},
-    permanentAddress:{ type:String, required:true},
-    guardian:guardianSchema,
-    localGuardian:localGuardianSchema,
+    email: { type: String, required:[true,'Email is required'], unique:true },
+    contactNo:{ type:String, required:[true,'ContactNo is required']},
+    emergencyContactNo:{ type:String, required:[true,'Emergency ContactNo is required']},
+    bloodGroup:{
+        type:String,
+        enum:['A+','A-','B+','B-','AB+','AB-','O+','O-'],
+    },
+    presentAddress:{ type:String, required:[true,'Present Address is required']},
+    permanentAddress:{ type:String, required:[true,'Permanent address is required']},
+    guardian:{
+        type:guardianSchema,
+        required:[true,'Guardian is required']
+    },
+    localGuardian:{
+        type:localGuardianSchema,
+        required:[true,'Local guardian is required']
+    },
     profileImg:{ type: String },
-    isActive:['active','blocked']
+    isActive:{
+        type:String,
+        enum:['active','blocked'],
+        default:'active',
+    }
 })
 
 export const StudentModel = model<TStudent>('Student', studentSchema);
