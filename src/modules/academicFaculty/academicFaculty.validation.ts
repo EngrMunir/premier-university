278ca1772d3526fce7c4b13764d88d737d93281b
munir <smsirajulmonir@gmail.com>
2025-01-17 09:49:27 +0600

    academic faculty route controller created

--- src/modules/academicFaculty/academicFaculty.validation.ts
+++ src/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,11 +1,17 @@
 import { z } from "zod";
 
-const academicFacultyValidationSchema = z.object({
+const createAcademicFacultyValidationSchema = z.object({
+    name:z.string({
+        invalid_type_error:'Academic faculty must be string',
+    }),
+});
+const updateAcademicFacultyValidationSchema = z.object({
     name:z.string({
         invalid_type_error:'Academic faculty must be string',
     }),
 });
 
-export const FacultyValidation ={
-    academicFacultyValidationSchema,
+export const AcademicFacultyValidation ={
+    createAcademicFacultyValidationSchema,
+    updateAcademicFacultyValidationSchema,
 };
