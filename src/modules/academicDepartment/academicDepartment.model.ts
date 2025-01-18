c5378b8292bc73f3e0405f94f36ea645b564d099
munir <smsirajulmonir@gmail.com>
2025-01-18 18:36:32 +0600

    detected error uncaughtexception unhandleRejection

--- src/modules/academicDepartment/academicDepartment.model.ts
+++ src/modules/academicDepartment/academicDepartment.model.ts
@@ -20,13 +20,13 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
 },
 );
 
-academicDepartmentSchema.pre('save', async function(next){
-    const isDepartmentExist = await AcademicDepartment.findOne({name: this.name})
-    if(isDepartmentExist){
-        throw new AppError(httpStatus.NOT_FOUND,'This department is already exist!!')
-    }
-    next();
-})
+// academicDepartmentSchema.pre('save', async function(next){
+//     const isDepartmentExist = await AcademicDepartment.findOne({name: this.name})
+//     if(isDepartmentExist){
+//         throw new AppError(httpStatus.NOT_FOUND,'This department is already exist!!')
+//     }
+//     next();
+// })
 
 
 academicDepartmentSchema.pre('findOneAndUpdate', async function(next){
