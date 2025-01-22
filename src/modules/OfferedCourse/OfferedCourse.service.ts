2b20525d348d2d64e748591bbb07a43958d9b20a
munir <smsirajulmonir@gmail.com>
2025-01-22 18:20:54 +0600

    time schedule conflict validate

--- src/modules/OfferedCourse/OfferedCourse.service.ts
+++ src/modules/OfferedCourse/OfferedCourse.service.ts
@@ -98,7 +98,55 @@ const getSingleOfferedCourseFromDB = async(id:string)=>{
 
 };
 
-const updateOfferedCourseIntoDB = async(id:string, payload:Partial<TOfferedCourse>)=>{};
+const updateOfferedCourseIntoDB = async(id:string, payload:Pick<TOfferedCourse,'faculty'|'days'|'startTime'|'endTime'>)=>{
+    const { faculty, days, startTime, endTime } = payload;
+
+    const isOfferedCourseExists = await OfferedCourse.findById(id);
+    if(!isOfferedCourseExists){
+        throw new AppError(httpStatus.NOT_FOUND, 'Offered course not found!');
+    }
+
+    const isFacultyExists = await Faculty.findById(faculty);
+
+    if(!isFacultyExists){
+        throw new AppError(httpStatus.NOT_FOUND, 'Faculty not found!');
+    }
+
+    const semesterRegistration = isOfferedCourseExists.semesterRegistration
+
+     // get the schedule of the faculties
+     const semesterRegistrationStatus = await SemesterRegistration.findById(semesterRegistration);
+
+     if(semesterRegistrationStatus?.status!=='UPCOMING'){
+        throw new AppError(
+            httpStatus.BAD_REQUEST,
+            `You can not update this offered course as it is ${semesterRegistrationStatus?.status}`,
+        )
+     }
+     const assignSchedules = await OfferedCourse.find({
+        semesterRegistration,
+        faculty,
+        days:{$in:days}
+    }).select('days startTime endTime');
+
+    const newSchedule = {
+        days,
+        startTime,
+        endTime
+    }
+
+   if(hasTimeConflict(assignSchedules,newSchedule)){
+    throw new AppError(
+        httpStatus.CONFLICT,
+        `This faculty is not available at that time! Choose other time or day`,
+    );
+   }
+
+   const result = await OfferedCourse.findByIdAndUpdate(id,payload,{
+    new:true,
+   });
+   return result;
+};
 
 
 export const OfferedCourseServices ={
